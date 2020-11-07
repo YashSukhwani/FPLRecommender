18f41b897fd45c3ff602175d2fbcbe1ee1dbd386
Yash Sukhwani <yash74sukhwani@gmail.com>
2020-11-07 04:25:01 -0600

    Backend Functions Complete - Connect with Frontend next

---
+++ fpl_reco/server_backup.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
+
+let app = express();
+let urlencodedPaser = bodyParser.urlencoded({
+  extended: false
+});
+
+const cors = require('cors');
+app.use(cors());
+
+app.use(express.json());
+
+// DB CONNECTION
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "fplrecommender_commonUser",
+  password: "Australia101"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+// API REQUESTS
+
+app.post('/abc', (req, res) => {
+  res.setHeader('content-type', 'text/plain');
+res.send(JSON.stringify({'Name':'B'}));
+});
+
+app.get('/', (req, res) => {
+  res.setHeader('content-type', 'text/plain');
+  res.send(JSON.stringify({'Name':'C'}));
+
+    let sql = `CALL getBest()`;
+
+    con.query(sql, true, (error, results, fields) => {
+    if (error) {
+      return console.error(error.message);
+    }
+    console.log(results[0]);
+  });
+
+});
+
+app.post('/home', (req, res) => {
+  res.setHeader('content-type', 'text/plain');
+  res.send(JSON.stringify({'Name':'A'}));
+});
+
+
+app.listen(3000);
+
+module.exports = app;
