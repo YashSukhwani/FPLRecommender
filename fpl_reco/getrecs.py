1bf440fe14231e215c040d56f46268c83fc93dbd
shrikar <shrikar2@illinois.edu>
2020-11-29 14:29:22 -0600

    fix rfregressor

--- fpl_reco/getrecs.py
+++ fpl_reco/getrecs.py
@@ -121,6 +121,7 @@ class DecisionTree():
 def main():
 	connection = connectDB()
 	tuples = runCommand(connection)
+	print(pd.DataFrame([item[3:18] for item in tuples]))
 	rfregressor = RandomForest(pd.DataFrame([item[3:18] for item in tuples]), np.array([item[-1] for item in tuples]), 15, 'sqrt', 592)
 	pred_scores = []
 	for i in tuples:
