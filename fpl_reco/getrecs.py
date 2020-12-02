11721133dfbc854e4b07bb3a9820785b2a609acf
shrikar <shrikar2@illinois.edu>
2020-12-02 01:36:28 -0600

    regressor prints json string output

--- fpl_reco/getrecs.py
+++ fpl_reco/getrecs.py
@@ -120,10 +120,10 @@ def main():
     rfregressor = RandomForest(pd.DataFrame(data=[item[3:18] for item in tuples], dtype=float), np.array([item[-1] for item in tuples]).astype(float), 15, 'sqrt', 592)
     pred_scores = rfregressor.predict(np.array([item[3:18] for item in tuples]).astype(float))
     scores_argsort = np.flip(np.argsort(pred_scores))
-    retval = "{'results':["
+    retval = '{"results":['
     for i in scores_argsort:
-    	retval += "{'name':'" + tuples[i][1] + " " + tuples[i][2] + "','position':'" + tuples[i][-4] + "','team':'" + tuples[i][-3] + "','status':'" + tuples[i][-2] + "','cost':" + tuples[i][-5] + "},"
-    retval = retval[:-1] + "]}"
+    	retval += '{"name":"' + tuples[i][1] + ' ' + tuples[i][2] + '","position":"' + tuples[i][-4] + '","team":"' + tuples[i][-3] + '","status":"' + tuples[i][-2] + '","cost":' + tuples[i][-5] + '},'
+    retval = retval[:-1] + ']}'
     print(retval)
 
 sys.path.insert(0, os.path.dirname(__file__))
